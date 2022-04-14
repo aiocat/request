ec0dce13be393aefa7089fe48b950b1deb219c09
aiocat <aiocat@protonmail.com>
2022-04-14 18:52:18 +0300

    remove scale style

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,7 +9,12 @@
 )]
 
 fn main() {
-  tauri::Builder::default()
+  let builder = tauri::Builder::default();
+  
+  #[cfg(target_os = "macos")]
+  let builder = builder.menu(menu::menu());
+
+  builder
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
